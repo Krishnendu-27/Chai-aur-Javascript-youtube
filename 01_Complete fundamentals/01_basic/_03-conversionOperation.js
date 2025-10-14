65c9d313e8ac3c68715c8870e959dbc55f965a99
Krishnendu-27 <kai27deep03@gmail.com>
2025-10-14 17:58:13 +0530

    added video summery

--- 01_Complete fundamentals/01_basic/_03-conversionOperation.js
+++ 01_Complete fundamentals/01_basic/_03-conversionOperation.js
@@ -1,6 +1,6 @@
 // we can check type 2 way
 let score = "52";
-console.log(typeof (score)); // with parenhesis
+console.log(typeof score); // with parenhesis
 console.log(typeof score); // without parenhesis
 
 /* type convertion */
@@ -16,11 +16,22 @@ console.log(newscore2);
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "krish"
+let isLoggedIn = "krish";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+// summury of video
+// datatype is condier nightmare in js . when value come from backend we dont know what is the type of the variable
+// thats why we use typeof  for change the type we use
+// convertor
+// example;
+// let val = "5";
+// let newval = Number(val);
+// if string have alphabate its return NaN but show its data type is Number 
+// if have bull after conversion it give 0 
+// when number covert string it return llok like number but itys string we can find out via typeof 
